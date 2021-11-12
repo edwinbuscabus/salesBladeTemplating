6bda3a26e9dfa5ba2a9ec36aaf337b44f412b2d0
edwin <edwinbuscabus.amoy@gmail.com>
2021-11-12 18:11:20 +0800

    first commit

---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,40 @@
+@extends('pages.base')
+
+
+@section('content')
+    
+
+<div class="jumbotron hero text-center">
+  <h2>Welcome!</h2>
+</div>
+
+<div class="container">
+  <div class="card-deck">
+
+            <div class="card card1">
+              <div class="card-body">
+                <h5 class="card-title text-center">About Us</h5>
+                <p class="card-text">“Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.”</p>
+              </div>
+            </div>
+
+            <div class="card card2">
+              <div class="card-body">
+                <h5 class="card-title text-center">Contact Us</h5>
+                <p class="card-text">“Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.”</p>
+              </div>
+            </div>
+
+            <div class="card card3">
+              <div class="card-body">
+                <h5 class="card-title text-center">Q&A</h5>
+                <p class="card-text">“Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.”</p>
+              </div>
+            </div>
+
+  </div>
+</div>
+
+
+
+@endsection
